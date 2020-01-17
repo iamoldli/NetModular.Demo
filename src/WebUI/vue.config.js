e56c03692cfb2fe06b81e30ec4fd5308f1a58005
Liwj <wenju1991@163.com>
2020-01-17 15:20:58 +0800

    update

--- src/WebUI/vue.config.js
+++ src/WebUI/vue.config.js
@@ -8,7 +8,7 @@ process.env.VUE_APP_COPYRIGHT = '版权所有：尼古拉斯·老李 | 用代码
 process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYYMDHHmmss')
 // 打包输出路径
 const outputDir = '../WebHost/wwwroot/app'
-const publicPath = isDev ? '' : 'https://static.nm.iamoldli.com/demo/'
+const publicPath = isDev ? '' : 'https://demo.nm.iamoldli.com/'
 
 module.exports = {
   outputDir,
