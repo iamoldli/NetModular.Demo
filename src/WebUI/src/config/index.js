f8d2b5ca51373f1c8ceb64fbfc72186624f83c2d
Liwj <wenju1991@163.com>
2020-03-03 14:12:00 +0800

    update

--- src/WebUI/src/config/index.js
+++ src/WebUI/src/config/index.js
@@ -1,7 +1,12 @@
 const isDev = process.env.NODE_ENV !== 'production'
 
 const config = {
-  baseUrl: 'https://api.demo.17mkh.com/api/'
+  baseUrl: 'https://api.demo.17mkh.com/api/',
+  /** 账户类型 */
+  accountTypes: [
+    { label: '管理员', value: 0 },
+    { label: '员工', value: 1 }
+  ]
 }
 
 // 开发模式
--- src/WebUI/src/main.js
+++ src/WebUI/src/main.js
@@ -2,6 +2,7 @@ import WebHost from 'netmodular-module-admin'
 import Common from 'netmodular-module-common'
 import CodeGenerator from 'netmodular-module-code-generator'
 import Quartz from 'netmodular-module-quartz'
+import PersonnelFiles from 'netmodular-module-personnelfiles'
 
 import config from './config'
 
@@ -9,6 +10,7 @@ import config from './config'
 WebHost.registerModule(Common)
 WebHost.registerModule(CodeGenerator)
 WebHost.registerModule(Quartz)
+WebHost.registerModule(PersonnelFiles)
 
 // 启动
 WebHost.start(config)
